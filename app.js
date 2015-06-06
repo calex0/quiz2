61cb9fe5f1d6395a46de9740a5c0e01ba7b020d7
Carlos <carlosalej.perez@gmail.com>
2015-06-06 12:08:52 +0200

    anadido pagina [creditos]

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-
+//var home = require('./routes/inicio');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -16,6 +16,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
+//app.use(image(__dirname + '/public/images/carlos.jpg'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
